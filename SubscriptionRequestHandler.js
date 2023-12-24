c9f72f99124b0ba57cd6aa4b8910e38f0ada7ce7
RyanBeGell <Ryanbegell@outlook.com>
2023-12-23 20:24:53 -0500

    email verification token

--- SubscriptionRequestHandler.js
+++ SubscriptionRequestHandler.js
@@ -1,26 +1,24 @@
 const AWS = require('aws-sdk');
 const SES = new AWS.SES();
 const DynamoDB = new AWS.DynamoDB.DocumentClient();
+const crypto = require('crypto');
 
 exports.handler = async (event) => {
-    // Parse email from event
     const { email } = JSON.parse(event.body);
+    const verificationToken = crypto.randomBytes(16).toString('hex');
 
-    // Store in DynamoDB
+    // Store in DynamoDB with verification token
     await DynamoDB.put({
         TableName: 'BlogSubscribers',
-        Item: { email, isSubscribed: false }
+        Item: { email, isSubscribed: false, verificationToken }
     }).promise();
 
-    // Generate a verification token 
-    const verificationToken = 'some_generated_token'; // TODO: Replace with actual token generation logic
+    // Construct verification link, env variable stored in AWS
+    const verificationLink = `${process.env.VERIFICATION_LINK_BASE_URL}/verify?token=${verificationToken}`;
 
-    // Construct the verification link (URL of your endpoint for EmailVerificationHandler)
-    const verificationLink = `https://your-verification-endpoint.com/verify?token=${verificationToken}`;
-
-    // Prepare SES email parameters
+    // SES email parameters
     const params = {
-        Source: 'your-email@example.com', //TODO:  Replace with your verified SES email
+        Source: 'noreply@ryanbegell.com',
         Destination: { ToAddresses: [email] },
         Message: {
             Subject: { Data: 'Blog Subscription Verification' },
@@ -30,7 +28,7 @@ exports.handler = async (event) => {
         }
     };
 
-    // Send verification email
+    // Send email
     await SES.sendEmail(params).promise();
 
     return { statusCode: 200, body: JSON.stringify({ message: 'Subscription initiated' }) };
