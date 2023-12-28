fb519e0053c533550fe6b028e47c3675770a48d3
RyanBeGell <Ryanbegell@outlook.com>
2023-12-27 19:41:38 -0500

    Add email validation and error handling to subscription request handler

--- SubscriptionRequestHandler.js
+++ SubscriptionRequestHandler.js
@@ -5,31 +5,52 @@ const crypto = require('crypto');
 
 exports.handler = async (event) => {
     const { email } = JSON.parse(event.body);
+
+    // Validate email
+    if (!email || !validateEmail(email)) {
+        return { statusCode: 400, body: JSON.stringify({ message: 'Invalid email address' }) };
+    }
+
     const verificationToken = crypto.randomBytes(16).toString('hex');
 
-    // Store in DynamoDB with verification token
-    await DynamoDB.put({
-        TableName: 'BlogSubscribers',
-        Item: { email, isSubscribed: false, verificationToken }
-    }).promise();
+    // Check environment variable
+    if (!process.env.VERIFICATION_LINK_BASE_URL) {
+        return { statusCode: 500, body: JSON.stringify({ message: 'Server configuration error' }) };
+    }
 
-    // Construct verification link, env variable stored in AWS
-    const verificationLink = `${process.env.VERIFICATION_LINK_BASE_URL}/verify?token=${verificationToken}`;
+    try {
+        // Store in DynamoDB with verification token
+        await DynamoDB.put({
+            TableName: 'BlogSubscribers',
+            Item: { email, isSubscribed: false, verificationToken }
+        }).promise();
 
-    // SES email parameters
-    const params = {
-        Source: 'noreply@ryanbegell.com',
-        Destination: { ToAddresses: [email] },
-        Message: {
-            Subject: { Data: 'Blog Subscription Verification' },
-            Body: {
-                Text: { Data: `Please click the following link to verify your subscription: ${verificationLink}` }
+        // Construct verification link
+        const verificationLink = `${process.env.VERIFICATION_LINK_BASE_URL}/verify?token=${verificationToken}`;
+
+        // SES email parameters
+        const params = {
+            Source: 'noreply@ryanbegell.com',
+            Destination: { ToAddresses: [email] },
+            Message: {
+                Subject: { Data: 'Blog Subscription Verification' },
+                Body: {
+                    Text: { Data: `Please click the following link to verify your subscription: ${verificationLink}` }
+                }
             }
-        }
-    };
+        };
 
-    // Send email
-    await SES.sendEmail(params).promise();
+        // Send email
+        await SES.sendEmail(params).promise();
 
-    return { statusCode: 200, body: JSON.stringify({ message: 'Subscription initiated' }) };
+        return { statusCode: 200, body: JSON.stringify({ message: 'Subscription initiated' }) };
+    } catch (error) {
+        console.error(error);
+        return { statusCode: 500, body: JSON.stringify({ message: 'Internal server error' }) };
+    }
 };
+
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
