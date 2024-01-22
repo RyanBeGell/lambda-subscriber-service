e85772cf713613b7b3ff25b525c2dc4f033b48ed
RyanBeGell <Ryanbegell@outlook.com>
2024-01-21 22:51:12 -0500

    Updated CORS headers

--- SubscriptionRequestHandler.js
+++ SubscriptionRequestHandler.js
@@ -57,8 +57,10 @@ exports.handler = async (event) => {
     return {
       statusCode: 200,
       headers: {
+        "Access-Control-Allow-Headers" : "Content-Type",
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Credentials': true,
+        "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
       },
       body: JSON.stringify({ message: 'Subscription initiated' }),
     };
