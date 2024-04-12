7fb55b7e95fb6afea783d6390a4ced967db3c39b
RyanBeGell <Ryanbegell@outlook.com>
2024-04-12 14:20:45 -0400

    Plain text email template swapped for HTML

--- SubscriptionRequestHandler.js
+++ SubscriptionRequestHandler.js
@@ -14,7 +14,7 @@ exports.handler = async (event) => {
     };
   }
 
-  const verificationToken =  crypto.randomUUID();
+  const verificationToken = crypto.randomUUID();
 
   // Check environment variable
   if (!process.env.VERIFICATION_LINK_BASE_URL) {
@@ -44,8 +44,38 @@ exports.handler = async (event) => {
       Message: {
         Subject: { Data: 'Blog Subscription Verification' },
         Body: {
-          Text: {
-            Data: `Please click the following link to verify your subscription: ${verificationLink}\n\nTo unsubscribe, click here: ${unsubscribeLink}`,
+          Html: {
+            Data: `
+            <html>
+            <head>
+              <style>
+                body { font-family: Arial, sans-serif; margin: 20px; color: black; }
+                .button { background-color: #2196F3; color: white; padding: 10px 20px; text-align: center; display: inline-block; font-size: 16px; margin: 4px 2px; cursor: pointer; text-decoration: none; border-radius: 5px; }
+                .header { font-size: 32px; margin-top: 20px; font-weight: 700; }
+                .footer { font-size: 14px; color: #777; margin-top: 20px; }
+                .unsubscribe { font-size: 14px; color: #555; margin-top: 5px; text-decoration: none; color: #2196F3; }
+              </style>
+            </head>
+            <body>
+              <center>
+                <img src="https://raw.githubusercontent.com/RyanBeGell/portfolio-site/main/public/favicon.png" height="64" "Logo" style="margin-bottom: 8px;">
+                <div class="header">Please Confirm Your Subscription</div>
+                <br/>
+                <br/>
+                <a href="${verificationLink}" class="button">Yes, subscribe me to this list</a>
+                <br/>
+                <br/>
+                <p class="footer">
+                  If you received this email by mistake, simply delete it. You won't be subscribed if you don't click the confirmation link above.
+                </p>
+                <p class="footer">
+                  To unsubscribe, <a href="${unsubscribeLink}" class="unsubscribe">click here</a>.
+                </p>
+              </center>
+            </body>
+            </html>
+            
+            `,
           },
         },
       },
@@ -57,10 +87,10 @@ exports.handler = async (event) => {
     return {
       statusCode: 200,
       headers: {
-        "Access-Control-Allow-Headers" : "Content-Type",
+        'Access-Control-Allow-Headers': 'Content-Type',
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Credentials': true,
-        "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+        'Access-Control-Allow-Methods': 'OPTIONS,POST,GET',
       },
       body: JSON.stringify({ message: 'Subscription initiated' }),
     };
